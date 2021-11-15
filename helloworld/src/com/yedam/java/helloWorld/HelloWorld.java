dffebb23a05239f2be59ef081353ab6df27ddeb0
admin <admin@YD03-20>
2021-11-15 16:26:40 +0900

    first commit

---
+++ helloworld/src/com/yedam/java/helloWorld/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.yedam.java.helloWorld;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO 컴퓨터에 실행을 요청할 명령어
+		System.out.println("Hello World!");
+	}
+}
