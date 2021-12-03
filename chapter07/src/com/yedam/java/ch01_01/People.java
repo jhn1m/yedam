3ffd11e999767ac07a835937f75d4e45bd6437d8
jhn1m <qqoxmaos@gmail.com>
2021-12-03 12:38:48 +0900

    12:38

---
+++ chapter07/src/com/yedam/java/ch01_01/People.java
@@ -0,0 +1,15 @@
+package com.yedam.java.ch01_01;
+
+public class People {
+	// 필드
+	public String name;
+	public String ssn;
+
+	// 생성자
+	public People(String name, String ssn) {
+		this.name = name;
+		this.ssn = ssn;
+	}
+
+	// 메서드
+}
---
+++ chapter07/src/com/yedam/java/ch01_02/C.java
@@ -0,0 +1,9 @@
+package com.yedam.java.ch01_02;
+
+public class C {
+	public void method() {
+//		A a = new A();
+//		a.field = "Value";
+//		a.method();
+	}
+}
