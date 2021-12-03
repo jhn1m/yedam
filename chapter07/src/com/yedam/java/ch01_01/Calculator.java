cd5943fe17421ae20fd8a69496aed28d6aae90c4
jhn1m <qqoxmaos@gmail.com>
2021-12-03 12:38:19 +0900

    12:38

--- chapter07/src/com/yedam/java/ch01_01/Calculator.java
+++
@@ -1,8 +0,0 @@
-package com.yedam.java.ch01_01;
-
-public class Calculator {
-	double areaCircle(double r) {
-		System.out.println("Calculator 객체의 areaCircle() 호출");
-		return 3.14156 * r * r;
-	}
-}
--- chapter07/src/com/yedam/java/ch01_02/C.java
+++
@@ -1,9 +0,0 @@
-package com.yedam.java.ch01_02;
-
-public class C {
-	public void method() {
-//		A a = new A();
-//		a.field = "Value";
-//		a.method();
-	}
-}
