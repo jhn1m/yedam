a4d13c84db91267b927fefcfbf7b41f2b3521221
jhn1m <qqoxmaos@gmail.com>
2021-12-15 11:08:12 +0900

    11:08

---
+++ chapter17_JDBC_02/src/com/yedam/java/app02/DeptDAO.java
@@ -0,0 +1,16 @@
+package com.yedam.java.app02;
+
+public class DeptDAO {
+	// 필드
+	// Oracle DB 정보, 각 메서드에서 공통 사용 변수
+
+	// SingleTon
+
+	// JDBC DRIVER, DB CONN
+
+	// Statement OR PreparedStatement
+
+	// SQL
+
+	// RESULT
+}
