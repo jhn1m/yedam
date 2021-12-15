c2ad267d2ed423eb274d41b8ff89b0c745c1e72d
jhn1m <qqoxmaos@gmail.com>
2021-12-15 10:27:34 +0900

    10:27

--- chapter17_JDBC_02/src/com/yedam/java/app01/EmpApp.java
+++ chapter17_JDBC_02/src/com/yedam/java/app01/EmpApp.java
@@ -1,17 +1,88 @@
 package com.yedam.java.app01;
 
+import java.sql.Date;
 import java.util.List;
+import java.util.Scanner;
 
 public class EmpApp {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		boolean run = true;
+
+		// DAO
 		EmpDAO dao = EmpDAO.getInstance();
-		List<Employee> datas = dao.selectAll();
+		Employee emp = null;
+		int employeeId = 0;
 
-		for (Employee data : datas) {
-			System.out.println(data);
+		while (run) {
+			System.out.println("---------------------------------------------------------------------");
+			System.out.println("| 1. 등록 | 2. 수정 | 3. 삭제 | 4. 사원조회 | 5. 전체조회 | 9. 종료 |");
+			System.out.println("---------------------------------------------------------------------");
+			System.out.print("선택 >> ");
+			int menuNo = sc.nextInt();
+			switch (menuNo) {
+			case 1:
+				emp = new Employee();
+				System.out.print("사번 >> ");
+				emp.setEmployeeId(sc.nextInt());
+				System.out.print("이름 >> ");
+				emp.setFirstName(sc.next());
+				System.out.print("성 >> ");
+				emp.setLastName(sc.next());
+				System.out.print("이메일 >> ");
+				emp.setEmail(sc.next());
+				System.out.print("전화번호 >> ");
+				emp.setPhoneNumber(sc.next());
+				System.out.print("입사일 >> ");
+				// 데이트타입 변환주의
+				emp.setHireDate(Date.valueOf(sc.next()));
+				System.out.print("직급 >> ");
+				emp.setJobId(sc.next());
+				System.out.print("연봉 >> ");
+				emp.setSalary(sc.nextDouble());
+				System.out.print("상여 >> ");
+				emp.setCommissionPct(sc.nextDouble());
+				System.out.print("상사 아이디 >> ");
+				emp.setManagerId(sc.nextInt());
+				System.out.print("부서번호 >> ");
+				emp.setDepartmentId(sc.nextInt());
+
+				dao.insert(emp);
+
+				break;
+			case 2:
+				emp = new Employee();
+				System.out.print("수정하려는 사번 >> ");
+				emp.setEmployeeId(sc.nextInt());
+				System.out.print("연봉을 얼마로 수정할까요? >> ");
+				emp.setSalary(sc.nextDouble());
+
+				dao.update(emp);
+				break;
+			case 3:
+				System.out.print("삭제하려는 사번 >> ");
+				emp = dao.selectOne(sc.nextInt());
+				System.out.println(emp);
+				break;
+			case 4:
+				System.out.print("조회하려는 사번 >> ");
+				emp = dao.selectOne(sc.nextInt());
+				System.out.println(emp);
+				break;
+			case 5:
+				List<Employee> list = dao.selectAll();
+				for (Employee employee : list) {
+					System.out.println(employee);
+				}
+				break;
+
+			case 9:
+				run = false;
+				sc.close();
+				System.out.println("프로그램을 종료합니다.");
+				break;
+			}
 		}
-
 	}
-
 }
--- chapter17_JDBC_02/src/com/yedam/java/app01/EmpDAO.java
+++ chapter17_JDBC_02/src/com/yedam/java/app01/EmpDAO.java
@@ -38,7 +38,7 @@ public class EmpDAO {
 
 	// 메서드
 
-	// DRIVER
+	// JDBC DRIVER LOADING, DBCONN
 	public void connect() {
 		try {
 			Class.forName(jdbc_driver);
@@ -49,8 +49,6 @@ public class EmpDAO {
 			System.out.println("DB 서버 접속에 실패했습니다.");
 		}
 	}
-
-	// DBCONN
 
 	// Statement OR PreparedStatement 객체 생성 // SQL
 	// 연산
@@ -101,10 +99,128 @@ public class EmpDAO {
 		}
 		return list;
 	}
+
+	// 단건조회
+	public Employee selectOne(int employeeId) {
+		Employee emp = new Employee();
+		// emp 값을 담음
+		try {
+			// JDBC DRIVER LOADING, DBCONN
+			connect();
+
+			// Statement 생성
+			stmt = conn.createStatement();
+
+			// SQL
+			String select = "SELECT * FROM employees WHERE employee_id = " + employeeId;
+			rs = stmt.executeQuery(select);
+
+			// RESULT
+			while (rs.next()) {
+				emp.setEmployeeId(rs.getInt("employee_id"));
+				emp.setFirstName(rs.getString("first_name"));
+				emp.setLastName(rs.getString("last_name"));
+				emp.setEmail(rs.getString("email"));
+				emp.setPhoneNumber(rs.getString("phone_number"));
+				emp.setHireDate(rs.getDate("hire_date"));
+				emp.setJobId(rs.getString("job_id"));
+				emp.setSalary(rs.getDouble("salary"));
+				emp.setCommissionPct(rs.getDouble("commission_pct"));
+				emp.setManagerId(rs.getInt("manager_id"));
+				emp.setDepartmentId(rs.getInt("department_id"));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			// 자원해제
+			disconnect();
+		}
+		return emp;
+	}
+
 	// INSERT
+	public void insert(Employee emp) {
+		try {
+			// JDBC DRIVER LOADING, DBCONN
+			connect();
+
+			// SQL
+			String insert = "INSERT INTO employees VALUES(?,?,?,?,?,?,?,?,?,?,?)";
+
+			// Preparedstatement 생성
+			pstmt = conn.prepareStatement(insert);
+
+			pstmt.setInt(1, emp.getEmployeeId());
+			pstmt.setString(2, emp.getFirstName());
+			pstmt.setString(3, emp.getLastName());
+			pstmt.setString(4, emp.getEmail());
+			pstmt.setString(5, emp.getPhoneNumber());
+			pstmt.setDate(6, emp.getHireDate());
+			pstmt.setString(7, emp.getJobId());
+			pstmt.setDouble(8, emp.getSalary());
+			pstmt.setDouble(9, emp.getCommissionPct());
+			pstmt.setInt(10, emp.getManagerId());
+			pstmt.setInt(11, emp.getDepartmentId());
+
+			// RESULT
+			int result = pstmt.executeUpdate();
+			System.out.println(result + "건이 등록되었습니다.");
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			// 자원해제
+			disconnect();
+		}
+	}
 
 	// UPDATE
+	public void update(Employee emp) {
+		try {
+			// JDBC DRIVER LOADING, DBCONN
+			connect();
+
+			// SQL
+			String update = "UPDATE employees SET salary = ? WHERE employee_id = ?";
+
+			// Preparedstatement 생성
+			pstmt = conn.prepareStatement(update);
+
+			pstmt.setDouble(1, emp.getSalary());
+			pstmt.setInt(2, emp.getEmployeeId());
+
+			// RESULT
+			int result = pstmt.executeUpdate();
+			System.out.println(result + "건이 수정되었습니다.");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			// 자원해제
+			disconnect();
+		}
+	}
 
 	// DELETE
+	public void delete(int employeeId) {
+		try {
+			// JDBC DRIVER LOADING, DBCONN
+			connect();
 
+			// SQL
+			String delete = "DELETE FROM employees WHERE employee_id = ?";
+
+			// Preparedstatement 생성
+			pstmt = conn.prepareStatement(delete);
+			pstmt.setInt(1, employeeId);
+
+			// RESULT
+			int result = pstmt.executeUpdate();
+			System.out.println(result + "건이 수정되었습니다.");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			// 자원해제
+			disconnect();
+		}
+	}
 }
